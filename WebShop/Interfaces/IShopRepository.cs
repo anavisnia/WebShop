978bcd8533dcf7d47a1b999167cf3c6e4aaa7d12
Ana Višnevskaja <anavisnia@gmail.com>
2021-06-24 18:01:25 +0300

    shop repository added

---
+++ WebShop/Interfaces/IShopRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebShop.Dtos;
+using WebShop.Entities;
+
+namespace WebShop.Repositories
+{
+    public interface IShopRepository
+    {
+        Task Add(ShopDto shopDto);
+        Task Delete(int id);
+        Task<List<ShopDto>> GetAll();
+        Task<Shop> GetById(int id);
+    }
+}
--- WebShop/Repositories/ShopRepository.cs
+++ WebShop/Repositories/ShopRepository.cs
@@ -1,21 +1,66 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebShop.Data;
+using WebShop.Dtos;
+using WebShop.Interfaces;
+using WebShop.Entities;
 
 namespace WebShop.Repositories
 {
-    public class ShopRepository
+    public class ShopRepository : IShopRepository
     {
         private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-        public ShopRepository(DataContext context)
+        public ShopRepository(DataContext context, IMapper mapper)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        public async Task<List<ShopDto>> GetAll()
+        {
+            var entities = await _context.Shops.ToListAsync();
 
+            return _mapper.Map<List<ShopDto>>(entities);
+        }
+
+        public async Task Add(ShopDto shopDto)
+        {
+            var shop = _mapper.Map<Shop>(shopDto);
+
+            _context.Add(shop);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Shop> GetById(int id)
+        {
+            var shop = _context.Set<Shop>().FirstOrDefault(s => s.Id == id);
+
+            if (shop == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return shop;
+        }
+
+        public async Task Delete(int id)
+        {
+            var shop = _context.Set<Shop>().FirstOrDefault(s => s.Id == id);
+
+            if (shop != null)
+            {
+                _context.Remove(shop);
+            }
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
